55761ff7ce578387dc1598589eb861a692d4d385
Dana Simmons <dsanalog@gmail.com>
2019-09-30 19:17:51 -0400

    hoookup app to mongo
    added connect string and dotfile
    to handle mongo db connection string

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -38,4 +40,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000);
+mongoose.connect(process.env.DB)
+.then( result => {
+  app.listen(3000);
+});
