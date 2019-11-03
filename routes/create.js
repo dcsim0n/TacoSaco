61aebe02a25e03aef5ae797a08a6e3d146909e61
Dana Simmons <dsanalog@gmail.com>
2019-11-03 09:13:57 -0500

    Add authentication middleware to creation routes

--- routes/create.js
+++ routes/create.js
@@ -1,20 +1,20 @@
 /*
 | Router for /create 
 */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const createController = require('../controllers/createController');
+const createController = require("../controllers/createController");
+const isAuth = require("../middleware/is-auth");
 
-
-router.get('/ingredients/new', createController.newIngredient )
-router.get('/ingredients/edit/:id', createController.editIngredient )
-router.get('/tacos/new', createController.newTaco )
-router.get('/tacos/edit/:id', createController.editTaco )
-router.post('/ingredients', createController.createIngredient )
-router.patch('/ingredients', createController.updateIngredient )
-router.post('/tacos', createController.createTaco )
-router.patch('/tacos', createController.updateTaco )
-router.get('/', createController.createMenu )
+router.get("/ingredients/new", isAuth, createController.newIngredient);
+router.get("/ingredients/edit/:id", isAuth, createController.editIngredient);
+router.get("/tacos/new", isAuth, createController.newTaco);
+router.get("/tacos/edit/:id", isAuth, createController.editTaco);
+router.post("/ingredients", isAuth, createController.createIngredient);
+router.patch("/ingredients", isAuth, createController.updateIngredient);
+router.post("/tacos", isAuth, createController.createTaco);
+router.patch("/tacos", isAuth, createController.updateTaco);
+router.get("/", createController.createMenu);
 
 module.exports = router;
